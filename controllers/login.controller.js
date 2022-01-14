6a4831ee32972c28f50faf57dbc4f4232c38cf5b
Maximiliano Pisso <maximiliano.pisso@gmail.com>
2022-01-13 21:46:59 -0300

    API LISTA !

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,34 +1,105 @@
-import  jwt  from "jsonwebtoken";
-import {SECRET_KEY} from "../config/config.js"
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../config/config.js"
 
 
+const usersContent = [
+    {
+        id: 1,
+        nombre: "Admin",
+        apellido: "Lopez",
+        direccion: "Rosario 1234",
+        movil: "+54 3416346585",
+        email: "maximiliano.pisso@gmail.com",
+        password: "12345678",
+        role: "admin"
+    },
+
+    {
+        id: 2,
+        nombre: "User",
+        apellido: "Perez",
+        direccion: "Corrientes 154",
+        movil: "+54 3416556688",
+        email: "user@gmail.com",
+        name: "USER - Normal",
+        password: "11223344",
+        role: "user"
+    }
+];
+
+//METODO GET - TRAE TODOS LOS USUARIOS
+export const getUsers = (req, res) => {
+    res.send(usersContent);
+}
+
+// METODO POST - VALIDACION DE USUARIOS
 export const validateCredentials = (req, res) => {
 
     const credentials = req.body;
+    const indexUser = usersContent.findIndex(user => user.email === credentials.email)
+    
+    console.log("Index:",indexUser);
+    console.log("Credenciales HTML", credentials.email, credentials.password);
 
-    if (credentials.user === 'test' && credentials.password === 'test'){
-
-        const payload ={
-            user: credentials.user,
-            userName: 'Test User',
-            mail: 'test_user@gmail.com',
-            role: 'admin'
+    if (indexUser >= 0 && usersContent[indexUser].password === credentials.password) {
+        console.log("USER VALIDO");
+        const payload = {
+            email: usersContent[indexUser].email,
+            nombre: usersContent[indexUser].name,
+            password: usersContent[indexUser].password,
+            role: usersContent[indexUser].role
         }
 
-        const token = jwt.sign(payload,SECRET_KEY);
+        const token = jwt.sign(payload, SECRET_KEY);
 
         res.json({
             status: 'OK',
-            token: token
+            token: token,
+            message: 'Usuario Validado',
         })
 
 
-    }else{
+    } else {
+        console.log("USER NO VALIDO");
         res.json({
             status: 'NOT OK',
             message: 'Invalid user or password'
         })
     }
-
-
 }
+
+export const addUser = (req, res) => {
+    const userToAdd = req.body;
+    const index = usersContent.findIndex(user => user.email === userToAdd.email) 
+      
+    if (index < 0) {
+        
+        usersContent.push({
+            id: usersContent.length+1,
+            nombre: userToAdd.nombre,
+            apellido: userToAdd.apellido,
+            direccion: userToAdd.direccion,
+            movil: userToAdd.movil,
+            email: userToAdd.email,
+            password: userToAdd.password,
+            role: "user"
+        });  
+
+        res.send({
+            status: "OK",
+            description: "ADD USER",
+            usersContent
+        });
+    } else {
+        res.send({
+            status: "NOT OK",
+            description: "NOT ADDING - USER ALLREADY EXIST",
+            usersContent
+        });
+    }
+}
+
+
+
+
+
--- routes.js
+++ routes.js
@@ -1,52 +1,59 @@
 
 import { addToCart, clearCart, getCart, removeFromCart } from "./controllers/cart.controller.js"
 import { getTest } from "./controllers/test.controller.js"
-import { validateCredentials } from "./controllers/login.controller.js"
-import  jwt  from "jsonwebtoken";
-import {SECRET_KEY} from "./config/config.js";
+import { addUser, getUsers, validateCredentials } from "./controllers/login.controller.js"
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "./config/config.js";
 import express from "express";
 
-export const routes = (app) =>{
-    
+export const routes = (app) => {
+
     app.route('/api/test')
-    .get(getTest)
+        .get(getTest)
 
     app.route('/api/cart')
-    .get(getCart)                 // obtiene contenido del carrito
-    .post(addToCart)              // agrega una movie al carrito
-    .delete(removeFromCart)       // elimina una movie del carrito
+        .get(checkToken, getCart)                 // obtiene contenido del carrito
+        .post(checkToken, addToCart)              // agrega una movie al carrito
+        .delete(checkToken, removeFromCart)       // elimina una movie del carrito
 
     app.route('/api/cart/clear')  // metodo para vaciar carrito
-    .delete(clearCart)
-      
+        .delete(clearCart)
 
     app.route('/api/login')
-    .post(validateCredentials)
+        .get(getUsers)                //
+        .post(addUser)                //
+
+    app.route('/api/login/validate')
+        .post(validateCredentials)   //
 }
 
-// const checkToken = express.Router(); 
-// checkToken.use((req, res, next) => {
-//     const token = req.headers['Authorization'];
-	
-//     if (token) {
-//       jwt.verify(token, SECRET_KEY, (err, decoded) => {      
-//         if (err) {
-//             return res.json({ 
-//                 status: 'NOK',
-//                 mensaje: 'Token inválido' 
-//             });    
-//         } else {
-//           req.decoded = decoded;    
-//           next();
-//         }
-//       });
-//     } else {
-//       res.send({ 
-//         status: 'NOK',
-//         mensaje: 'Token no provisto' 
-//       });
-//     }
-//  });
+
+const checkToken = express.Router();  
+checkToken.use((req, res, next) => {
+
+    let token = req.headers['authorization'];
+    token = token.replace('Bearer ', '')
+
+    if (token) {
+        jwt.verify(token, SECRET_KEY, (err, decoded) => {
+            if (err) {
+                return res.json({
+                    status: 'NOT - OK',
+                    mensaje: 'Token inválido'
+                });
+            } else {
+               
+                req.decoded = decoded;    
+                next();
+            }
+        });
+    } else {
+        res.send({
+            status: 'NOT - OK',
+            mensaje: 'Token no provisto'
+        });
+    }
+});
 
 
 // const getTest = (req,res) =>{
